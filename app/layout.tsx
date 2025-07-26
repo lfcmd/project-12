4b65bf49d734fa4bca124af7de3f14a0677b0708
lfcmd.github.io <1783376585@qq.com>
2025-07-26 13:14:54 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { GeistSans } from 'geist/font/sans';
 import "./globals.css";
 import Footer from '@/components/footer';
+import Script from "next/script";
+
 
 export const metadata: Metadata = {
   title: "HandDraw.AI - 专业手绘风格AI生成平台",
@@ -15,6 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-black text-white">
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JW3WQ9XKX7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JW3WQ9XKX7');
+</script>
         <main>{children}</main>
         <Footer />
         <div id="portal-root"></div>
